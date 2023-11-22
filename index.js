1358dbbb6d2e3a6d0916728271c632d4e4e183ab
Shabeer <shabeerwebdev@gmail.com>
2023-11-23 00:23:00 +0530

    pushed

--- index.js
+++ index.js
@@ -1,33 +1,68 @@
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// let isApiBusy = false;
+
+// const sloFn = () => {
+//   return setTimeout(() => {
+//     isApiBusy = false;
+//   }, 20000);
+// };
+
+// app.get("/my-api", (req, res) => {
+//   const clientIP =
+//     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+//   console.log("clientIP is here", clientIP, isApiBusy, "clientIP is here");
+//   if (!isApiBusy) {
+//     isApiBusy = true;
+//     try {
+//       const ok = sloFn();
+//       res.send(ok);
+//     } catch (err) {
+//       console.log(err, "err is here");
+//       res.status(500).send("Internal Server Error");
+//     }
+//   } else {
+//     console.log("api is busy, try later");
+//     res.send("api is busy, try later");
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 let isApiBusy = false;
 
-const sloFn = () => {
-  return setTimeout(() => {
-    resolve("your api is success in 5s");
-    isApiBusy = false;
-  }, 20000);
-};
-
-app.get("/my-api", (req, res) => {
-  const clientIP =
-    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-  console.log("clientIP is here", clientIP, isApiBusy, "clientIP is here");
+// Middleware to check if the API is busy
+const apiGuard = (req, res, next) => {
   if (!isApiBusy) {
     isApiBusy = true;
-    try {
-      const ok = sloFn();
-      res.send(ok);
-    } catch (err) {
-      console.log(err, "err is here");
-      res.status(500).send("Internal Server Error");
-    }
+    next();
   } else {
-    console.log("api is busy, try later");
-    res.send("api is busy, try later");
+    res.status(503).send("API is busy, please try again later.");
+  }
+};
+
+// Your endpoint
+app.get("/my-api", apiGuard, async (req, res) => {
+  try {
+    // Your API logic goes here
+
+    // Simulating some async operation
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Send a response
+    res.send("Your API response");
+  } finally {
+    // Release the API lock
+    isApiBusy = false;
   }
 });
 
