1741c57bfdff095c02270f8947917ca24c6dd0a3
Shabeer <shabeerwebdev@gmail.com>
2023-11-23 19:45:38 +0530

    pushed

--- index.js
+++ index.js
@@ -5,26 +5,19 @@ let isApiBusy = false;
 
 app.get("/endpointAB", async (req, res) => {
   if (isApiBusy) {
+    console.log(isApiBusy, "isApiBusy");
     return res.status(503).json({ message: "API busy, try again later." });
   }
 
-  // Set the flag to indicate that the API is busy
   isApiBusy = true;
 
   try {
-    // Simulate a time-consuming process (5 minutes in this case)
     await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    // Your actual processing logic goes here
-
-    // Send the response when processing is complete
     res.json({ message: "Processing complete" });
   } catch (error) {
-    // Handle errors here
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   } finally {
-    // Reset the flag to indicate that the API is no longer busy
     isApiBusy = false;
   }
 });
